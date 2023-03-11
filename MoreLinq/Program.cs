15115bb0e4bc43fe2d877b17d184f6b86dc66ae9
iman-97 <iman1095@yahoo.com>
2023-03-11 21:36:32 +0330

    Add project files.

---
+++ MoreLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoreLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Func<int>>
+            {
+                () => 2 + 2,
+                () => 2 * 5,
+                () => 4 * 4,
+                () => (int)Math.Pow(8, 2)
+            };
+
+            foreach (var item in list.Evaluate())
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
